3c497788007390621965d1700df9fb4f8c9704bf
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-09-11 23:14:30 +0300

    Добавляет блок Программы

--- source/js/modules/swiper/init-swiper.js
+++ source/js/modules/swiper/init-swiper.js
@@ -1,4 +1,5 @@
 const swiperContainerHero = document.querySelector('.hero__container');
+const swiperContainerPrograms = document.querySelector('.programs__container');
 
 const initSwiper = () => {
   const swiperHero = new Swiper(swiperContainerHero, {
@@ -19,6 +20,39 @@ const initSwiper = () => {
       delay: 3000,
     },*/
     cssMode: true,
+  });
+
+  const swiperPrograms = new Swiper(swiperContainerPrograms, {
+    /* pagination: {
+      el: '.programs__pagination',
+      type: 'bullets',
+      bulletElement: 'button',
+      bulletClass: 'programs__bullet',
+      bulletActiveClass: 'hero__bullet--active',
+      clickable: true,
+    }, */
+    navigation: {
+      nextEl: '.programs__button--next',
+      prevEl: '.programs__button--prev',
+    },
+    simulateTouch: false,
+    scrollbar: {
+      el: '.programs__scrollbar',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
+    },
   });
 };
 
