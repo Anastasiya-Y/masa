a8ce178974888caa310625c008de6bff8d6ec922
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-09-12 17:00:15 +0300

    Дорабатывает блок Программы

--- source/js/modules/swiper/init-swiper.js
+++ source/js/modules/swiper/init-swiper.js
@@ -23,33 +23,36 @@ const initSwiper = () => {
   });
 
   const swiperPrograms = new Swiper(swiperContainerPrograms, {
-    /* pagination: {
-      el: '.programs__pagination',
-      type: 'bullets',
-      bulletElement: 'button',
-      bulletClass: 'programs__bullet',
-      bulletActiveClass: 'hero__bullet--active',
-      clickable: true,
-    }, */
     navigation: {
-      nextEl: '.programs__button--next',
-      prevEl: '.programs__button--prev',
+      nextEl: '.programs__nav-button--next',
+      prevEl: '.programs__nav-button--prev',
     },
     simulateTouch: false,
     scrollbar: {
       el: '.programs__scrollbar',
+      dragSize: '392px',
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
         spaceBetween: 10,
+        scrollbar: {
+          hide: true,
+          enabled: false,
+        },
       },
       768: {
         slidesPerView: 'auto',
         spaceBetween: 30,
+        scrollbar: {
+          dragSize: '324px',
+        },
       },
       1200: {
         slidesPerView: 3,
+        spaceBetween: 12,
+      },
+      1240: {
         spaceBetween: 32,
       },
     },
